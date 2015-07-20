7fbad5fede435b267d118bb2e299a2df7d32e7c2
Jeff <jmaj.email@gmail.com>
2015-07-20 14:55:18 -0400

    fixed the bg scrolling

--- js/Panel.js
+++ js/Panel.js
@@ -5,7 +5,14 @@ function Panel(bg_canvas, overlay_canvas, pegs) {
 	I.overlay_canvas = overlay_canvas;
 	I.showSpecialPeg = false;
 	I.pegs = pegs;
-	I.speed = 15;
+	I.speed = .25;
+	I.scrollVal = 0;
+	I.bg_image = null;
+
+	I.startY = 0;
+	I.startY2 = -8100
+
+
 
 	I.splatAll = function() {
 		this.pegs.forEach(function(peg) {
@@ -18,18 +25,30 @@ function Panel(bg_canvas, overlay_canvas, pegs) {
 	}
 
 	I.moveBG = function(totalSeconds, img, val) {
-		this.bg_canvas.getContext('2d').save();
-    	this.bg_canvas.getContext('2d').translate(0, this.xPosition(totalSeconds, img.height));
-    	this.bg_canvas.getContext('2d').drawImage(img, 0, val);
-    	this.bg_canvas.getContext('2d').restore();
+
+
+		this.bg_canvas.getContext('2d').drawImage(img,0,this.startY);
+		this.bg_canvas.getContext('2d').drawImage(img,0,this.startY2);
+
+		if (this.startY > img.height) {
+		    this.startY = -(img.height-1);
+		}
+		
+		if (this.startY2 > img.height) {
+		   this.startY2 = -(img.height-1);
+		}
+
+		  this.startY += this.speed;
+		  this.startY2 += this.speed;
 	}
 
 	I.drawPegs = function(mousePos) {
 		this.overlay_canvas.getContext("2d").clearRect(0, 0, this.overlay_canvas.width, this.overlay_canvas.height);
 	    
+	    var that = this;
 	    this.pegs.forEach(function(peg) {
 	    	peg.draw();
-	    	peg.y += .25;
+	    	peg.y += that.speed;
 
 	    	var mX = mousePos.x;
 		    var mY = mousePos.y;
