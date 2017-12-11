a0381a0ab135eeac08f51d1d0f6f6bcc95464e6e
Matt Love <mattlovedesign@gmail.com>
2017-12-11 15:39:26 -0500

    chop-dance integration

---
+++ dance/js/button.js
@@ -0,0 +1,32 @@
+function Button(file, x, y) {
+
+		this.x = x;
+		this.y = y;
+
+		this.arrayIndex = 0;
+
+	  var pixels = [];
+
+		this.canvas = uiCanvas;
+		this.context = uiCanvas.getContext('2d');
+
+	  var tcanvas = document.createElement('canvas'), /// create temp canvas
+	  tctx = tcanvas.getContext('2d'); /// temp context
+
+	  this.img = new Image();
+
+	  this.img.onload = function () {
+	  }
+	  this.img.crossOrigin = "Anonymous";
+	  this.img.src = file;
+
+		this.draw = function() {
+	    this.context.save();
+	    //console.log(this.img);
+	    this.context.drawImage(this.img, this.x, this.y);
+	    this.context.restore();
+		}
+
+
+		return this;
+	}
